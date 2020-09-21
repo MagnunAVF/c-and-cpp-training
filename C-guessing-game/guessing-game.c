955416e90c3b968f3af9d4ffabdcb2df3b8c5d76
Magnun A V F <magnunavf@gmail.com>
2020-09-21 01:41:46 -0300

    Add feedback to user

--- C-guessing-game/guessing-game.c
+++ C-guessing-game/guessing-game.c
@@ -13,4 +13,18 @@ int main()
     printf("What's your guess? ");
     scanf("%d", &user_guess);
     printf("Your guess is %d.\n", user_guess);
+
+    int is_correct_guess = (user_guess == secret_number);
+    if(is_correct_guess) {
+        printf("Correct guessing!\n");
+    } else {
+        printf("Wrong ! Try again.\n");
+
+        int is_greater_than_secret = (user_guess > secret_number);
+        if(is_greater_than_secret) {
+            printf("Your guess is greater than the secret number\n");
+        } else {
+            printf("Your guess is less than the secret number\n");
+        }
+    }
 }
