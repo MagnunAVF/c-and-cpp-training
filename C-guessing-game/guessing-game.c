b263f70b667cbb63abbac5f3d30c177fc9d74ccd
Magnun A V F <magnunavf@gmail.com>
2020-09-24 14:39:01 -0300

    Add score to game
    - Also create a main version.

---
+++ C-guessing-game/guessing-game.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("*****************************\n");
+    printf("* Welcome to Guessing Game! *\n");
+    printf("*****************************\n");
+
+    int secret_number = 42;
+    int tries = 1;
+    double score = 1000;
+
+    while (1)
+    {
+        printf("\nTry %d.\n", tries);
+
+        int user_guess;
+        printf("What's your guess (Digit a number)? ");
+        scanf("%d", &user_guess);
+        printf("Your guess is %d.\n", user_guess);
+
+        if (user_guess < 0) {
+            printf("Your number cannot be negative!\n");
+        }
+
+        int is_correct_guess = (user_guess == secret_number);
+        int is_greater_than_secret = (user_guess > secret_number);
+
+        if (is_correct_guess)
+        {
+            printf("Correct guess!\n");
+            break;
+        }
+        else if (is_greater_than_secret)
+        {
+            printf("Wrong ! Try again.\n");
+            printf("Your guess is greater than the secret number\n");
+        }
+        else
+        {
+            printf("Wrong ! Try again.\n");
+            printf("Your guess is less than the secret number\n");
+        }
+
+        tries++;
+
+        double score_lost = abs(user_guess - secret_number) / (double)2;
+        score = score - score_lost;
+    }
+
+    printf("\nEnd of the game! \n");
+    printf("Number of guesses: %d.\n", tries);
+    printf("Game Score: %.3f\n", score);
+}
